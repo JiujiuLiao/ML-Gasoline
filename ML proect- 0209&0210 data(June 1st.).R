c1449a5dd8dc6042f240eef1a79b7145cd6b11b1
JiujiuLiao <jiujiu.liao@outlook.com>
2024-06-01 14:00:29 +0200

    Update ML proect- 0209&0210 data(June 1st.).R

--- ML proect- 0209&0210 data(June 1st.).R
+++ ML proect- 0209&0210 data(June 1st.).R
@@ -14,28 +14,34 @@ library(lubridate)
 
 
 
+#set work directory
 setwd("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Price dataset Feb.09th - Mar.07th")
 setwd("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Station dataset Feb.09th - Mar.07th")
-#########combine prices data of day 0209 and 0210#############
+
+#######################################################################################
+###############################pre-processing data#####################################
+#######################################################################################
+
+#combine prices data of day 0209 and 0210
 prices_0209th <- read.csv("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Price dataset Feb.09th - Mar.07th/2024-02-09-prices.csv")
 prices_0210th <- read.csv("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Price dataset Feb.09th - Mar.07th/2024-02-10-prices.csv")
 prices_combined <- rbind(prices_0209th,prices_0210th)
 
-#########combine stations data of day 0209 and 0210#############
+#combine stations data of day 0209 and 0210
 stations_0209th <- read.csv("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Station dataset Feb.09th - Mar.07th/2024-02-09-stations.csv")
 stations_0210th <- read.csv("C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/Station dataset Feb.09th - Mar.07th/2024-02-10-stations.csv")
 stations_combined <- rbind(stations_0209th,stations_0210th)
 
 
-#########preprocess unreasonable data#############
+#delete unreasonable data
 p_sub_df <- subset(prices_combined, diesel > 0& e5 > 0& e10 > 0)
 s_sub_df <- data.frame(subset(stations_combined, 
                latitude > 0 & longitude > 0 &
                uuid != "5f15982b-5505-4ea2-def3-f0c94afee70d" &
                uuid != "e70fb59b-3e7d-4ece-9e98-256206f0aec3"))
-#########change station_uuid into uuid to combine stations data set and prices data set#############
-names(p_df)[2] ="uuid"
-prices_stations_combined <- data.frame( merge(p_df,s_df, by= "uuid", all.x  = FALSE, all.y = FALSE, sort =TRUE))
+
+names(p_sub_df)[2] ="uuid"  #change station_uuid into uuid to combine stations data set and prices data set
+prices_stations_combined <- data.frame( merge(p_sub_df,s_sub_df, by= "uuid", all.x  = FALSE, all.y = FALSE, sort =TRUE)) #merge two data sets into one data set
 
 # select only date and diesel columns
 date_diesel_data <- select(prices_stations_combined, date, diesel)
@@ -48,7 +54,11 @@ ts.date_diesel_data <- xts(date_diesel_data$diesel,
 # inspect the object
 str(ts.date_diesel_data)
 
-#########visualize the price of diesel#############
+#######################################################################################
+###########################visualize the price of diesel###############################
+#######################################################################################
+
+##method 1 choose one random price in each hour, and see the trend
 #random seed
 set.seed(99)
 
@@ -74,25 +84,24 @@ selected_ts <- xts(selected_data, order.by = date_sequence)
 plot(selected_ts, main = "diesel price by randomly choosing", xlab = "date", ylab = "diesel")
 
 
-#export pdf
+##method 2 calculate the mean of price in each hour, and see the trend
+# Convert xts object to data frame
+date_diesel_data <- data.frame(date = index(ts.date_diesel_data), diesel = coredata(ts.date_diesel_data))
 
+# Convert the date to hours
+date_diesel_data$date <- floor_date(date_diesel_data$date, "hour")
 
+# Calculate the average price for each hour
+hourly_avg <- date_diesel_data %>%
+  group_by(date) %>%
+  summarise(avg_price = mean(diesel, na.rm = TRUE))
 
-#pdf('C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/1.pdf')
-jpeg('C:/Users/jiuji/OneDrive/Desktop/study/Advanced method in data analysis/Applications in Data Analytics/2.pdf')
-
-
-ggsave()
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Use ggplot to plot the price trend
+ggplot(hourly_avg, aes(x = date, y = avg_price)) +
+  geom_line(color = "black") +  # Set the line color to blue
+  geom_point(color = "red") +  # Add red points to mark the average price for each hour
+  geom_vline(xintercept = as.numeric(as.POSIXct("2024-02-09 23:59:59", tz = "UTC")), linetype = "dashed", color = "yellow") +  # Add a yellow dashed line in the between of the day
+  scale_x_datetime(breaks = date_breaks("2 hours"), labels = date_format("%H:%M")) +  # Display the time every two hours
+  labs(x = "Date", y = "Average Price", title = "Diesel Price Trend from 2024-02-09 00:00:00 to 2024-02-10 23:59:59") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate the x-axis labels to avoid overlap
